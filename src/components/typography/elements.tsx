f71b329142f3d79082f33df41a6cb99793e5791c
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-02 22:35:59 -0700

    Fix SVG

--- src/components/typography/elements.tsx
+++ src/components/typography/elements.tsx
@@ -2,7 +2,7 @@ import type { ComponentProps } from "react"
 
 const Link = (props: ComponentProps<"svg">) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <line x1="4" x2="20" y1="9" y2="9" />
       <line x1="4" x2="20" y1="15" y2="15" />
       <line x1="10" x2="8" y1="3" y2="21" />
