e9a2da3987f4e94a6b9c916716cf8dcb7c251a66
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-02 21:29:19 -0700

    Add MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import svelte from "@astrojs/svelte";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), react(), svelte()]
+  }), react(), svelte(), mdx()]
 });
