6f0967aa9a652d0503677f8943d98ff28eacb538
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-04 12:33:39 -0700

    Lucide icon

--- src/components/header/ThemeSwitcher.svelte
+++ src/components/header/ThemeSwitcher.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { createDropdownMenu, melt } from '@melt-ui/svelte';
+  import {Sun, Moon} from 'lucide-svelte'
   const {
     elements: { menu, item, trigger },
   } = createDropdownMenu({
@@ -26,36 +27,8 @@
 </script>
 
 <button class="btn btn-sm btn-ghost btn-square" use:melt={$trigger}>
-  <!-- Sun -->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="w-4 h-4 dark:hidden"
-    ><circle cx="12" cy="12" r="4" /><path d="M12 2v2" /><path
-      d="M12 20v2"
-    /><path d="m4.93 4.93 1.41 1.41" /><path d="m17.66 17.66 1.41 1.41" /><path
-      d="M2 12h2"
-    /><path d="M20 12h2" /><path d="m6.34 17.66-1.41 1.41" /><path
-      d="m19.07 4.93-1.41 1.41"
-    /></svg
-  >
-  <!-- Moon -->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="w-4 h-4 hidden dark:block"
-    ><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" /></svg
-  >
+  <Sun class="w-4 h-4 dark:hidden" />
+  <Moon class="w-4 h-4 hidden dark:block" />
   <span class="sr-only">Toggle theme</span>
 </button>
 <ul class="menu bg-base-200 w-24 rounded-box" use:melt={$menu}>
