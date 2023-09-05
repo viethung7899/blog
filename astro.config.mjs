ed0c1dea21bcf5f75ba0f3f7c3f82dcce29952d0
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-05 09:07:16 -0700

    Update configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
+import { visit } from "unist-util-visit"
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,7 +19,22 @@ export default defineConfig({
     svelte(),
   ],
   markdown: {
+    extendDefaultPlugins: true,
+    syntaxHighlight: false,
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    rehypePlugins: [
+      rehypeKatex,
+      () => (tree) => {
+        visit(tree, (node) => {
+          if (node?.type === "element" && node?.tagName === "pre") {
+            const [codeEl] = node.children
+            if (codeEl.tagName !== "code") return
+            node.properties["raw"] = codeEl.children?.[0].value
+            node.properties["lang"] = codeEl.properties?.className?.[0]?.replace("language-", "")
+            node.properties["meta"] = codeEl.properties?.metastring
+          }
+        })
+      },
+    ],
   },
 });
