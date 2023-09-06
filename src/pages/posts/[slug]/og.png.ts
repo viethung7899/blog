9e233aa42cc6b4ef530b9e27de48747cdd94a8c6
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-05 23:06:34 -0700

    Add background to OG images

--- src/pages/posts/[slug]/og.png.ts
+++ src/pages/posts/[slug]/og.png.ts
@@ -1,7 +1,7 @@
-import Card from "@/components/og/Card.svelte"
 import { Resvg } from "@resvg/resvg-js"
 import type { APIContext } from "astro"
 import { getCollection, type CollectionEntry } from "astro:content"
+import { readFileSync } from "fs"
 import satori, { type Font } from "satori"
 import { html } from "satori-html"
 
@@ -14,6 +14,8 @@ export async function getStaticPaths() {
     },
   }))
 }
+
+const imageBase64 = Buffer.from(readFileSync("src/assets/background.png")).toString("base64")
 
 async function getFonts() {
   const regular = await fetch("https://fonts.cdnfonts.com/s/19795/Inter-Regular.woff").then((res) =>
@@ -56,6 +58,7 @@ interface Props {
 
 export async function GET({ props }: APIContext) {
   const { data } = props as Props
+
   const date = new Intl.DateTimeFormat("en-US", {
     year: "numeric",
     month: "long",
@@ -64,9 +67,16 @@ export async function GET({ props }: APIContext) {
   }).format(data.date)
   const tags = data.tags?.map((tag) => `#${tag}`) || []
 
-  // @ts-ignore
-  const render = Card.render({ data })
-  const markup = html(render.html)
+  const markup = html`
+  <div tw="w-full h-full flex flex-col justify-between p-16 text-white"
+    style="background: url('data:image/png;base64,${imageBase64}')">
+    <div tw="text-4xl font-bold" style="color: #1fb2a6;">V_</div>
+    <div tw="flex flex-col">
+      <div tw="text-4xl font-semibold">${data.title}</div>
+      <p tw="text-xl opacity-80">${[date, ...tags].join(' · ')}</p>
+    </div>
+  </div>
+  `
 
   const fonts = await getFonts()
 
