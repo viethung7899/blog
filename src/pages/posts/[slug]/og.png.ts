745caa6eacdcb7b4c519849645ee7ffea288964c
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-07 01:38:54 -0700

    Update OG generation

--- src/pages/posts/[slug]/og.png.ts
+++ src/pages/posts/[slug]/og.png.ts
@@ -1,7 +1,6 @@
 import { Resvg } from "@resvg/resvg-js"
-import type { APIContext } from "astro"
-import { getCollection, type CollectionEntry } from "astro:content"
-import { readFileSync } from "fs"
+import type { APIContext, InferGetStaticPropsType } from "astro"
+import { getCollection } from "astro:content"
 import satori, { type Font } from "satori"
 import { html } from "satori-html"
 
@@ -14,8 +13,6 @@ export async function getStaticPaths() {
     },
   }))
 }
-
-const imageBase64 = Buffer.from(readFileSync("src/assets/background.png")).toString("base64")
 
 async function getFonts() {
   const regular = await fetch("https://fonts.cdnfonts.com/s/19795/Inter-Regular.woff").then((res) =>
@@ -52,24 +49,25 @@ const DIMENSIONS = {
   height: 630,
 }
 
-interface Props {
-  data: CollectionEntry<"posts">["data"]
-}
+type Props = InferGetStaticPropsType<typeof getStaticPaths>;
 
-export async function GET({ props }: APIContext) {
+export async function GET({ props, url }: APIContext) {
   const { data } = props as Props
 
   const date = new Intl.DateTimeFormat("en-US", {
     year: "numeric",
-    month: "long",
+    month: "short",
     day: "numeric",
     timeZone: "UTC",
   }).format(data.date)
   const tags = data.tags?.map((tag) => `#${tag}`) || []
 
+  const backgroundURL = new URL("og-background.png", url.origin)
+
   const markup = html`
   <div tw="w-full h-full flex flex-col justify-between p-16 text-white"
-    style="background: url('data:image/png;base64,${imageBase64}')">
+    style="background: url('${backgroundURL}')"
+  >
     <div tw="text-4xl font-bold" style="color: #1fb2a6;">V_</div>
     <div tw="flex flex-col">
       <div tw="text-4xl font-semibold">${data.title}</div>
