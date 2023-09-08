afe9143bb8c7c1a7745c2f1c3dc09cfabd47239f
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-08 15:42:40 -0700

    Analyser

--- src/interactives/audio/audio.ts
+++ src/interactives/audio/audio.ts
@@ -1,8 +1,18 @@
 import { writable } from "svelte/store"
 
-const FFT_SIZE = 256
-const SAMPLES = 256
-
 export const audioFile = writable<File>()
 export const progress = writable(0)
 export const playing = writable(false)
+export const analyser = writable<AnalyserNode>()
+
+export const FFT_SIZE = 256
+
+export function setupAnalyzer(audio: HTMLAudioElement) {
+  const context = new AudioContext()
+  const source = context.createMediaElementSource(audio)
+  const analyserNode = context.createAnalyser()
+  analyserNode.fftSize = FFT_SIZE
+  source.connect(analyserNode)
+  analyserNode.connect(context.destination)
+  analyser.set(analyserNode)
+}
