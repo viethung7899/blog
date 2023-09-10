427053a69c7ebc2d05628c996d1d25f27e133f9a
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-10 15:06:02 -0700

    Remove Melt UI

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import { preprocessMeltUI } from "@melt-ui/pp"
-import sequence from "svelte-sequential-preprocessor"
 import { vitePreprocess } from "@astrojs/svelte"
+
 export default {
-  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+  preprocess: vitePreprocess(),
 }
