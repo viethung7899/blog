ced5637c04d48e831d5b5af1a33747131aa1ce6f
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-10 20:52:44 -0700

    Remove thumbnail

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,6 @@ const postCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.date(),
-    thumbnail: z.string(),
     description: z.string(),
     interactive: z.boolean().default(false),
     tags: z.array(z.string()).optional(),
