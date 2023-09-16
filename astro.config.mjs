c80d9c75cae679312dae821f3675d00fbcc2d619
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-16 04:01:49 -0700

    Use Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,41 @@
-import mdx from "@astrojs/mdx"
-import react from "@astrojs/react"
-import svelte from "@astrojs/svelte"
-import tailwind from "@astrojs/tailwind"
-import { defineConfig } from "astro/config"
-import rehypeKatex from "rehype-katex"
-import remarkMath from "remark-math"
-import { visit } from "unist-util-visit"
-import { remarkReadingTime } from "./remark-reading-time.mjs"
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import { visit } from "unist-util-visit";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.vhng.dev",
-  integrations: [
-    mdx(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    svelte(),
-  ],
+  integrations: [mdx(), tailwind({
+    applyBaseStyles: false
+  }), react(), svelte()],
   markdown: {
     extendDefaultPlugins: true,
     syntaxHighlight: false,
     remarkPlugins: [remarkMath, remarkReadingTime],
-    rehypePlugins: [
-      rehypeKatex,
-      () => (tree) => {
-        visit(tree, (node) => {
-          if (node?.type === "element" && node?.tagName === "pre") {
-            const [codeEl] = node.children
-            if (codeEl.tagName !== "code") return
-            node.properties["raw"] = codeEl.children?.[0].value
-            node.properties["lang"] = codeEl.properties?.className?.[0]?.replace("language-", "")
-            node.properties["meta"] = codeEl.properties?.metastring
-          }
-        })
-      },
-    ],
+    rehypePlugins: [rehypeKatex, () => tree => {
+      visit(tree, node => {
+        if (node?.type === "element" && node?.tagName === "pre") {
+          const [codeEl] = node.children;
+          if (codeEl.tagName !== "code") return;
+          node.properties["raw"] = codeEl.children?.[0].value;
+          node.properties["lang"] = codeEl.properties?.className?.[0]?.replace("language-", "");
+          node.properties["meta"] = codeEl.properties?.metastring;
+        }
+      });
+    }]
   },
-})
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  })
+});
