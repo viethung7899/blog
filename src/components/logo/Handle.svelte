1a6c5208ec69bdb896987fa4650f0cfbe67781a0
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-16 09:31:00 -0700

    Fancy logo animation

---
+++ src/components/logo/Handle.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let show = true
+  export let className: string | undefined
+</script>
+
+<span class={show ? "opacity-100" : `opacity-0 ${className || "mr-[-0.5em]"}`}>
+  <slot />
+</span>
+
+<style>
+  span {
+    transition:
+      opacity 500ms ease-in-out,
+      margin 500ms ease-in-out;
+  }
+</style>
